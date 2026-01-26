bd8b8e61ccb5eaf831aee5ee2d339b86ae20698d
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-26 23:29:30 +1100

    -Added prompt with system message and user message

--- 07-prompt-template/src/main/java/com/spring/ai/firstproject/config/AIConfig.java
+++ 07-prompt-template/src/main/java/com/spring/ai/firstproject/config/AIConfig.java
@@ -17,7 +17,7 @@ public class AIConfig {
                 .defaultOptions(OpenAiChatOptions.builder()
                         .model("gpt-3.5-turbo")
                         .temperature(0.7)
-                        .maxTokens(1000).build())
+                        .maxTokens(200).build())
                 .build();
     }
 }
--- 07-prompt-template/src/main/java/com/spring/ai/firstproject/service/ChatServiceImpl.java
+++ 07-prompt-template/src/main/java/com/spring/ai/firstproject/service/ChatServiceImpl.java
@@ -3,6 +3,7 @@ package com.spring.ai.firstproject.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -34,6 +35,8 @@ public class ChatServiceImpl implements ChatService {
     @Override
     public String chatTemplate(String message) {
 
+        /**
+         *
         //First step: Create a PromptTemplate
         PromptTemplate template = PromptTemplate.builder()
                 .template("As an expert in coding  and programming. Always write program in {tech name} Now reply for this question: {message}")
@@ -49,8 +52,25 @@ public class ChatServiceImpl implements ChatService {
         //Third step: Create a Prompt with the rendered message
         Prompt prompt = new Prompt(rendererMessage);
 
+         **/
+
+        SystemPromptTemplate systemPrompt = SystemPromptTemplate.builder()
+                .template("You are a helpful assistant that helps people find information.")
+                .build();
+        var systemMessage = systemPrompt.createMessage();
+
+        PromptTemplate userTemplate = PromptTemplate.builder()
+                .template("What is the {techname} ? Tell me a little intro about {techExample}")
+                .build();
+        var userMessage = userTemplate.createMessage(Map.of(
+                "techname", "Java",
+                "techExample", "Spring AI"
+        ));
+
+        Prompt finalPrompt = new Prompt(systemMessage, userMessage);
+
         return this.chatClient
-                .prompt(prompt)
+                .prompt(finalPrompt)
                 .call()
                 .content();
     }

a1d7f4c749996852d5d12588e519bca58d42d122
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-22 09:40:35 +1100

    -prompts using prompt and chatclient

--- 000-spring-ai/explore-openai/src/main/java/com/llm/chats/PromptController.java
+++ 000-spring-ai/explore-openai/src/main/java/com/llm/chats/PromptController.java
@@ -4,6 +4,7 @@ import com.llm.dto.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -31,5 +32,31 @@ public class PromptController {
         this.chatClient = chatClientBuilder.build();
     }
 
+    @PostMapping("/v1/prompts")
+    public String prompts(@RequestBody UserInput userInput) {
+        log.info("User input received: {}", userInput);
+
+        var  systemMessage = """
+        You are a helpful assistant who can answer JAVA based questions.
+        For any other questions, you will politely decline to answer and suggest the user to ask a JAVA based question.
+        """;
+
+       var sysMessage =  new SystemMessage(systemMessage);
+       var userMessage = new UserMessage(userInput.prompt());
+
+       var promptMessage = new Prompt(List.of(sysMessage,
+               new UserMessage("What's my name?"),
+               new AssistantMessage("I don't know"),
+               new UserMessage("My name is John"),
+               userMessage));
+
+        var response = chatClient
+                .prompt(promptMessage)
+                .call()
+                .content();
+
+        log.info("Response received: {}", response);
+        return response;
+    }
 
 }
